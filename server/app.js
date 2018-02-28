c8906c8c58a7e7bc637ceea2952dfe1a746d44d5
moon70717 <moon70717@gmail.com>
2018-02-28 11:22:19 +0900

    '호우'

--- server/app.js
+++ server/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
 
-app.use('/',(req,res,next)=>{
-    console.log(req.url);
-    if(req.url.indexOf('.html')!=-1){
-        res.sendFile(req.url,{root:__dirname});
-    }
-    res.sendFile('/index.html',{root:__dirname});
-});
+var initFunc = function (req, res) {
+    console.log('req =>', req);
+    console.log('res =>', res);
+    res.send('hello~');
+}
 
-app.listen(4003, function(){
+app.post('/',initFunc);
+
+// app.use('/',(req,res,next)=>{
+//     console.log(req.url);
+//     if(req.url.indexOf('.html')!=-1){
+//         res.sendFile(req.url,{root:__dirname});
+//     }
+//     res.sendFile('/index.html',{root:__dirname});
+// });
+
+app.listen(4003, function () {
     console.log("Express start : port[4003]");
 });
